6bac471ab9c2ca520af7a4e86c478dc0fa87c122
dellingc <35514357+dellingc@users.noreply.github.com>
2019-10-22 09:52:58 -0400

    added players GET, added callbacks to display games and players

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-import games from './apis/games';
+import trivia from './apis/trivia';
 
-games.get('/games')
-  .then(function (response) {
-    console.log(response);
+let gamesList = document.getElementById('games-list');
+let playerSelect = document.getElementById('player-select');
+
+let gamesArr = [];
+
+async function getGames(callback) {
+  const response = await trivia.get('/games');
+  console.log(response.data)
+  response.data.forEach((game) => {
+    gamesArr.push(game);
+    callback(game)
   })
-  .catch(function (error) {
-    console.log(error);
-  });
+ };
 
-//   getGames = async () => {
-//     const response = await games.get('/games') 
-//          console.log(response);
-//   };
+async function getPlayers(callback) {
+  const response = await trivia.get('/players');
+  console.log(response)
+  response.data.forEach((player) => {
+    callback(player);
+  })
+}
 
-//   getGames();
+function displayGames(game) {
+  let li = document.createElement("li");
+  li.innerHTML = game.game_date.substring(0, 10) + " - " + game.location_name + " - " + game.place + " - " + game.points;
+  gamesList.appendChild(li)
+}
+
+function displayPlayers(player) {
+  let opt = document.createElement("option");
+  opt.innerHTML = player.first_name + " " + player.last_name;
+  playerSelect.appendChild(opt);
+}
+
+getGames(displayGames);
+getPlayers(displayPlayers);
+
