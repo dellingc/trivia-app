21c398c4fda148e409f3f330ab181e27b88e41c5
dellingc <35514357+dellingc@users.noreply.github.com>
2019-10-22 10:20:47 -0400

    added locations api call and display

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import trivia from './apis/trivia';
 
 let gamesList = document.getElementById('games-list');
 let playerSelect = document.getElementById('player-select');
+let locationSelect = document.getElementById('location-select');
 
 let gamesArr = [];
 
@@ -22,6 +23,14 @@ async function getPlayers(callback) {
   })
 }
 
+async function getLocations(callback) {
+  const response = await trivia.get('/locations');
+  console.log(response)
+  response.data.forEach((location) => {
+    callback(location);
+  })
+}
+
 function displayGames(game) {
   let li = document.createElement("li");
   li.innerHTML = game.game_date.substring(0, 10) + " - " + game.location_name + " - " + game.place + " - " + game.points;
@@ -30,10 +39,19 @@ function displayGames(game) {
 
 function displayPlayers(player) {
   let opt = document.createElement("option");
+  opt.setAttribute("value", `${player.player_id}`);
   opt.innerHTML = player.first_name + " " + player.last_name;
   playerSelect.appendChild(opt);
 }
 
+function displayLocations(location) {
+  let opt = document.createElement("option");
+  opt.setAttribute("value", `${location.location_id}`);
+  opt.innerHTML = location.location_name;
+  locationSelect.appendChild(opt);
+}
+
 getGames(displayGames);
 getPlayers(displayPlayers);
+getLocations(displayLocations);
 
